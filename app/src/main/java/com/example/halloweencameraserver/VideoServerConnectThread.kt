c10d917bf8a5e61b30635095ac4e1b9c39a8621b
Marcella Schutz <athil023@protonmail.com>
2019-10-31 17:11:01 +0800

    Initial version of server

---
+++ app/src/main/java/com/example/halloweencameraserver/VideoServerConnectThread.kt
@@ -0,0 +1,78 @@
+package com.example.halloweencameraserver
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.os.Handler
+import android.util.Log
+import java.io.IOException
+import java.util.*
+
+class VideoServerConnectThread( device: BluetoothDevice, private val handler: Handler ) {
+    private val TAG = "JBVidCon"
+    //private val SPP_UUID = "00001101-0000-1000-8000-00805f9b34fb"
+    private val VIDEO_SERVER_UUID = "00001101-0000-1000-8000-00805f9b34ff"
+
+
+    val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private var connectThread : ConnectThread? = null
+
+    init {
+        connectThread = ConnectThread( device, handler )
+        connectThread?.start()
+    }
+
+    public var videoServerRunnerThread : VideoServerRunnerThread? = null
+
+    fun getRunner() : VideoServerRunnerThread? {
+        return videoServerRunnerThread
+    }
+
+    fun cancel() {
+        connectThread?.cancel()
+    }
+
+    private inner class ConnectThread(private val device: BluetoothDevice, var handler : Handler) : Thread() {
+
+        private var mmServerSocket: BluetoothServerSocket? = null
+
+
+        override fun run() {
+            // Keep listening until exception occurs or a socket is returned.
+            mmServerSocket = bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord("JBVIDEO", UUID.fromString(VIDEO_SERVER_UUID) )
+
+            var shouldLoop = true
+            while (shouldLoop) {
+                val socket: BluetoothSocket? = try {
+                    mmServerSocket?.accept()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Socket's accept() method failed", e)
+                    shouldLoop = false
+                    null
+                }
+                socket?.also {
+                videoServerRunnerThread = VideoServerRunnerThread()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+                videoServerRunnerThread?.connect(it, handler)
+
+                    mmServerSocket?.close()
+                    shouldLoop = false
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        fun cancel() {
+            videoServerRunnerThread?.disconnect()
+            videoServerRunnerThread = null
+            try {
+                mmServerSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/halloweencameraserver/ui/main/MainFragment.kt
@@ -0,0 +1,113 @@
+package com.example.halloweencameraserver.ui.main
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.halloweencameraserver.R
+import com.example.halloweencameraserver.VideoServerConnectThread
+
+class MainFragment : Fragment() {
+    private val TAG = "JBBTSERVER"
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // TODO: Use the ViewModel
+    }
+
+    private var videoServer: VideoServerConnectThread? = null
+
+    private fun connectBluetooth( ) {
+        val REQUEST_ENABLE_BT = 1459
+
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+        if  ( bluetoothAdapter != null ) {
+            if (bluetoothAdapter.isEnabled == false) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+
+            val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
+
+            var peer : BluetoothDevice? = null
+
+            if ( pairedDevices != null ) {
+                for (device in pairedDevices) {
+                    val deviceName = device.name
+                    //val deviceHardwareAddress = device.address // MAC address
+                    //val PEER_NAME = "JB_Canary"
+                    //val PEER_NAME = "JB_Flash"
+                    val PEER_NAME = "JB_Robin"
+
+                    if (device.name == PEER_NAME) {
+                        peer = device
+                        break
+                    }
+                }
+            }
+            if (peer != null ) {
+                startBluetoothThread()
+                videoServer = VideoServerConnectThread( peer, bluetoothHandler!! )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        connectBluetooth()
+
+        // When the screen is turned off and turned back on, the SurfaceTexture is already
+        // available, and "onSurfaceTextureAvailable" will not be called. In that case, we can open
+        // a camera and start preview from here (otherwise, we wait until the surface is ready in
+        // the SurfaceTextureListener).
+    }
+
+    override fun onPause() {
+        stopBluetoothThread()
+        videoServer?.cancel()
+        videoServer = null
+
+        super.onPause()
+    }
+
+    private var bluetoothThread : HandlerThread? = null
+    private var bluetoothHandler: Handler? = null
+
+    private fun startBluetoothThread() {
+        bluetoothThread = HandlerThread("Bluetooth")
+        bluetoothThread?.start()
+        bluetoothHandler = Handler( bluetoothThread!!.looper)
+    }
+
+    private fun stopBluetoothThread( ) {
+        bluetoothThread?.quitSafely()
+        try {
+            bluetoothThread?.join()
+            bluetoothThread = null
+            bluetoothHandler = null
+        } catch( e: InterruptedException ) {
+            Log.e( TAG,e.toString() )
+        }
+    }
+}
